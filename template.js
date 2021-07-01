7f6df5b3484de42c84dd8e1b1a27d4021ae46cb6
boke992 <86782799+boke992@users.noreply.github.com>
2021-07-02 03:17:41 +0900

    Delete template.js

--- template.js
+++
@@ -1,8 +0,0 @@
-function templatesAdd() {
-    $("#productTable tbody").append("<tr>" + 
-        "<td>My First Video</td>" +
-        "<td>6/11/2015</td>" +
-        "<td>www.pluralsight.com</td>" +
-        "</tr>");
-}
-

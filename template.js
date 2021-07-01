3ab00f3de1f5075fa8aca2c2e8e451a9152dc02f
myo213 <myo@ganasys.com>
2021-07-01 23:20:24 +0900

    my first line of code

---
+++ template.js
@@ -0,0 +1,8 @@
+function templatesAdd() {
+    $("#productTable tbody").append("<tr>" + 
+        "<td>My First Video</td>" +
+        "<td>6/11/2015</td>" +
+        "<td>www.pluralsight.com</td>" +
+        "</tr>");
+}
+
